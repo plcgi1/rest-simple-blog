322faf529bf4145543262e588d4772b4257d57de
pl <plcgi1@gmail.com>
2017-04-10 00:39:17 +0300

    rate limit

--- server/config/express.js
+++ server/config/express.js
@@ -16,39 +16,48 @@ var errorHandler = require('errorhandler');
 var path = require('path');
 var config = require('./environment');
 var passport = require('passport');
+var limit = require('express-better-ratelimit')
 
-module.exports = function(app) {
-  var env = app.get('env');
+module.exports = function (app) {
+	var env = app.get('env');
 
-  app.use(compression());
+	// rate limit
+	app.use(limit({
+		duration: 30000, //30 seconds
+		max: 100 // requests to server
+		//blackList: ['192.168.1.1']
+	}));
 
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+	app.use(compression());
 
-  app.use(express.static('client'));
+	// content negotiation
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(bodyParser.json());
 
-  app.use(methodOverride());
-  app.use(cookieParser());
-  app.use(passport.initialize());
+	app.use(express.static('client'));
 
-  app.use(session({
-      secret: config.secrets.session,
-      cookie: { maxAge: 20 * 60 * 1000 },
-      store: new MongoStore({
-          url: config.mongo.uri,
-          stringify: false, // default true, if true, connect-mongo will serialize sessions using JSON.stringify before setting, and deserialize with JSON.parse when getting
-          ttl: 20 * 60, // 20 min, 14 days default, when the session cookie has an expiration date, connect-mongo will use it, or it will use ttl option
-          autoRemove: 'interval', // remove expired sessions, using defined interval
-          autoRemoveInterval: 10 // 10 min default
-      })
-  }));
+	app.use(methodOverride());
+	app.use(cookieParser());
+	app.use(passport.initialize());
 
-  if ('production' === env) {
-    app.use(morgan('dev'));
-  }
+	app.use(session({
+		secret: config.secrets.session,
+		cookie: { maxAge: 20 * 60 * 1000 },
+		store: new MongoStore({
+			url: config.mongo.uri,
+			stringify: false, // default true, if true, connect-mongo will serialize sessions using JSON.stringify before setting, and deserialize with JSON.parse when getting
+			ttl: 20 * 60, // 20 min, 14 days default, when the session cookie has an expiration date, connect-mongo will use it, or it will use ttl option
+			autoRemove: 'interval', // remove expired sessions, using defined interval
+			autoRemoveInterval: 10 // 10 min default
+		})
+	}));
 
-  if ('development' === env || 'test' === env) {
-    app.use(morgan('dev'));
-    app.use(errorHandler()); // Error handler - has to be last
-  }
+	if ('production' === env) {
+		app.use(morgan('dev'));
+	}
+
+	if ('development' === env || 'test' === env) {
+		app.use(morgan('dev'));
+		app.use(errorHandler()); // Error handler - has to be last
+	}
 };
